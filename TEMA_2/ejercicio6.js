259934fc58e82cc2ba0ab12181ea98245a1f43c9
Manuel Muñoz <heidiaricel@hotmail.com>
2017-10-05 13:14:58 +0200

    Añadido Ejercicio 6 de la Unidad 2
    Se ha añadido el ejercicio 6 de la unidad 2, ademas se ha modificado el tamaño de las imagenes de otros apartados

---
+++ TEMA_2/ejercicio6.js
@@ -0,0 +1,10 @@
+
+function cargarElementos(){
+  var y = document.images.length;
+  document.write(y + '<br>');
+  document.write('<br>');
+  document.write(document.images.length + '<br/>');
+  document.write(document.images[0].id + '<br/>');
+  document.write(document.links.length + '<br/>');
+  document.title = 'Titulo cambiado';
+}
