6c76546962ccd96e98122836c18c22fe22b89e2b
Manuel Muñoz <heidiaricel@hotmail.com>
2017-10-05 19:24:32 +0200

    Corregidos algunos ejercicios
    Corregidos algunos ejercicios que no funcionaban o que faltaba algo por modificar y no me habia dado cuenta :(

--- TEMA_2/ejercicio6.js
+++ TEMA_2/ejercicio6.js
@@ -1,10 +1,12 @@
 
 function cargarElementos(){
+  /*
   var y = document.images.length;
   document.write(y + '<br>');
   document.write('<br>');
-  document.write(document.images.length + '<br/>');
-  document.write(document.images[0].id + '<br/>');
-  document.write(document.links.length + '<br/>');
+  */
+  document.write('Total de imagenes: ' + document.images.length + '<br/>');
+  document.write('Id de imagen: ' + document.images[0].id + '<br/>');
+  document.write('Total de enlaces: ' + document.links.length + '<br/>');
   document.title = 'Titulo cambiado';
 }
---
+++ TEMA_2/ejercicio7.js
@@ -0,0 +1,11 @@
+function testGeolocation() {
+  if ("geolocation" in navigator) {
+    document.write('La geolocalización esta disponible en el navegador <br/>');
+  } else {
+    document.write('La geolocalización no esta disponible en el navegador <br/>');
+  }
+}
+
+function posicionActual() {
+return 'hi';
+}
