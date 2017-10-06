29da0c2c6a1745cba6a787af740eaef713da984c
Manuel Muñoz <heidiaricel@hotmail.com>
2017-10-06 11:12:08 +0200

    Primera parte ejercicio geolocalizacion
    Subida inicial con el ejercicio de geolocalizacion

--- TEMA_2/ejercicio7.js
+++ TEMA_2/ejercicio7.js
@@ -1,3 +1,5 @@
+
+
 function testGeolocation() {
   if ("geolocation" in navigator) {
     document.write('La geolocalización esta disponible en el navegador <br/>');
@@ -7,5 +9,44 @@ function testGeolocation() {
 }
 
 function posicionActual() {
-return 'hi';
+  var output = document.getElementById("out-2");
+
+  if (!navigator.geolocation) {
+    output.innerHTML = "<p>La geolocalización esta disponible en el navegador</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+  }
+
+  function error() {
+    output.innerHTML = "La geolocalización no esta disponible en el navegador <br/>";
+  }
+  output.innerHTML = "<p>Cargando…</p>";
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function posicionActualDos() {
+  var output = document.getElementById("out-3");
+
+  if (!navigator.geolocation) {
+    output.innerHTML = "<p>La geolocalización esta disponible en el navegador</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+  }
+
+  function error() {
+    output.innerHTML = "La geolocalización no esta disponible en el navegador <br/>";
+  }
+  output.innerHTML = "<p>Cargando…</p>";
+  navigator.geolocation.getCurrentPosition(success, error);
 }
