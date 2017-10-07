2990aab0ab07023d3f740da77f21043e2ee40956
Manuel Muñoz <heidiaricel@hotmail.com>
2017-10-07 17:28:39 +0200

    Ejercicio 7 Unidad 2
    Ejercicio con la API de google, la clave para generar un mapa esta eliminada y no la subiré en proximas versiones

--- TEMA_2/ejercicio7.js
+++ TEMA_2/ejercicio7.js
@@ -1,5 +1,3 @@
-
-
 function testGeolocation() {
   if ("geolocation" in navigator) {
     document.write('La geolocalización esta disponible en el navegador <br/>');
@@ -49,4 +47,61 @@ function posicionActualDos() {
   }
   output.innerHTML = "<p>Cargando…</p>";
   navigator.geolocation.getCurrentPosition(success, error);
+}
+
+//Apartado 2 - 1
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 6
+  });
+  var map2 = new google.maps.Map(document.getElementById('map2'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 6
+  });
+  var infoWindow = new google.maps.InfoWindow({
+    map: map
+  });
+  var marker = new google.maps.Marker({
+    map: map2,
+    draggable: true,
+    position: {
+      lat: -34.397,
+      lng: 150.644
+    }
+  });
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Te encontré');
+      map.setCenter(pos);
+      marker.setPosition(pos);
+      map2.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
 }
