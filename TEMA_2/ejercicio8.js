57d857af67c2d4e33f45965e2799bb6e6b7f5a18
Manuel Muñoz <heidiaricel@hotmail.com>
2017-10-09 18:09:23 +0200

    Ejercicio 8 unidad 2
    Ejercicio 8 unidad 2 terminado, ya captura audio y video

--- TEMA_2/ejercicio8.js
+++ TEMA_2/ejercicio8.js
@@ -1,15 +1,16 @@
-function capturaVideo()
- {
+function capturaVideo() {
   function handleVideo(stream) {
     video.src = window.URL.createObjectURL(stream);
   }
 
   function videoError(e) {
-    alert('Se ha producido un error');
+    alert('Se ha producido un error' + e);
   }
   var video = document.querySelector("#videoElement");
-  var constraints = {video: true};
-  
+  var constraints = {
+    video: true
+  };
+
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
   if (navigator.getUserMedia) {
@@ -17,7 +18,24 @@ function capturaVideo()
   }
 
 }
-
 function capturaAudio() {
-  console.log('test');
+
+  function audioError(e) {
+    alert('Se ha producido un error' + e);
+  }
+
+  function cogeAudio(stream) {
+    var microphone = context.createMediaStreamSource(stream);
+    var filter = context.createBiquadFilter();
+    // microphone -> filter -> destination.
+    microphone.connect(filter);
+    filter.connect(context.destination);
+  }
+  var constraints = {
+    audio: true
+  };
+  
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  var context = new AudioContext();
+  navigator.getUserMedia(constraints, cogeAudio, audioError);
 }
