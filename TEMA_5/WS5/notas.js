06be88e7e7a46910b8cf2871a2b1c8fe03256f29
Manuel Muñoz <heidiaricel@hotmail.com>
2017-11-21 12:47:11 +0100

    Sticky Notes V1
    - Crear los elementos Notas
    - Crear los elementos NotasVIstas

--- TEMA_5/WS5/notas.js
+++ TEMA_5/WS5/notas.js
@@ -1,19 +1,61 @@
+var contador = 0;
+var contenedor = "";
 class Nota {
-  constructor(titulo="", texto="", hora="") {
+  constructor(titulo = "", texto = "") {
+    this.id = "nota" + (++contador);
     this.titulo = titulo;
     this.texto = texto;
-    this.hora = hora;
+    this.hora = new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
   }
 }
 
+
 class NotaVista {
-  constructor(id="") {
-    this.id=id;
+  constructor(id = "") {
+    var temp = "";
+    var nota = "";
+    nota = document.createElement('div');
+    temp = document.createAttribute("id");
+    temp.value = id;
+    nota.setAttributeNode(temp);
+    contenedor.appendChild(nota);
+    this.id = document.getElementById(id);
+    this.nuevaNota();
+  }
+  nuevaNota() {
+    this.id.style.backgroundColor = "#ffd60a";
+    this.id.style.cssFloat = "left";
+    this.id.style.width = "300px";
+    this.id.style.height = "300px";
+    this.id.style.border = "solid black";
+    this.id.style.margin = "5px";
+
   }
 }
 
 class Controlador {
   constructor() {
-    this.contenedor = [];
+    this.contenedorNotas = [];
+    this.contenedorVistas = [];
   }
+  anadirNotas(objetoNota = "") {
+    this.contenedorNotas.push(objetoNota);
+  }
+  anadirVistas(objetoVista = "") {
+    this.contenedorVistas.push(objetoVista);
+  }
+}
+
+
+
+window.onload = function() {
+  contenedor = document.getElementById('contenedorNotas');
+  var notas = new Controlador();
+  notas.anadirNotas(new Nota('Prueba 1', 'El texto'));
+  notas.anadirNotas(new Nota('Prueba 2', 'El texto'));
+  console.log(notas.contenedorNotas);
+  notas.anadirVistas(new NotaVista(notas.contenedorNotas[0].id));
+  notas.anadirVistas(new NotaVista(notas.contenedorNotas[1].id));
+  console.log(notas.contenedorVistas);
+
 }
