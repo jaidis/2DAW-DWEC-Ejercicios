cd1c043cd416822d29e2b5d73f8be5e1aed5b051
Manuel Muñoz <heidiaricel@hotmail.com>
2018-01-02 19:12:08 +0100

    OMDB v0.1
    - Permite la busqueda de un tittulo de una pelicula
    - Si la pelicula existe se crea un objeto Pelicula y se almacena en un array
    - Si existen varias paginas las recorre con el boton Mas hasta su maximo

---
+++ PROYECTOS/PROYECTO_01/js/api_omdb.js
@@ -0,0 +1,134 @@
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// + Iniciar variables globales
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log('Iniciando variables globales');
+var apiKey = '9d074a1f';
+var busqueda = 'lego';
+var jsonBusqueda = [];
+var jsonPelicula = [];
+var temp = '';
+var contadorPaginas = 1;
+var maxPagina = 100;
+var contenedorPeliculas = [];
+
+// console.log('Variables globales cargadas');
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// + Definición de clases
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class Pelicula {
+  constructor(titulo = '', fecha = '', imdbID = '', poster = '') {
+    this.titulo = titulo;
+    this.fecha = fecha;
+    this.imdbID = imdbID;
+    this.poster = poster;
+  }
+  extendido(calificacion = '', duracion = '', genero = '', director = '', sinopsis = '', puntuacion = '') {
+    this.calificacion = calificacion;
+    this.duracion = duracion;
+    this.genero = genero;
+    this.director = director;
+    this.sinopsis = sinopsis;
+    this.puntuacion = puntuacion;
+  }
+}
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// + Funciones
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function ajaxBusqueda(pagina = 1, tipo = 'movie') {
+  // console.log('http://www.omdbapi.com/?apikey=' + apiKey + '&s=' + busqueda + '&type=movie&page=' + pagina);
+  $.ajax({
+      url: 'http://www.omdbapi.com/?apikey=' + apiKey + '&s=' + busqueda + '&type=movie&page=' + pagina,
+      type: 'GET',
+      dataType: 'json',
+      success: function(response) {
+        jsonBusqueda = JSON.stringify(response);
+        jsonBusqueda = JSON.parse(jsonBusqueda);
+        // console.log(jsonBusqueda);
+      }
+    })
+    .done(function() {
+      console.log("Busqueda Ajax Completada");
+      maxPagina = parseInt((parseInt(jsonBusqueda.totalResults) / 10) + 1);
+      guardaPeliculas();
+    })
+    .fail(function() {
+      console.log("error");
+    });
+}
+
+function guardaPeliculas() {
+  // console.log(jsonBusqueda.Search.length);
+  for (var i = 0; i < jsonBusqueda.Search.length; i++) {
+    // console.log(jsonBusqueda.Search[i].imdbID);
+    ajaxPelicula(jsonBusqueda.Search[i].imdbID);
+  }
+}
+
+function ajaxPelicula(imdbID) {
+  $.ajax({
+      url: 'http://www.omdbapi.com/?apikey=' + apiKey + '&i=' + imdbID + '&plot=full',
+      type: 'GET',
+      dataType: 'json',
+      success: function(response) {
+        jsonPelicula = JSON.stringify(response);
+        jsonPelicula = JSON.parse(jsonPelicula);
+        // console.log(jsonPelicula);
+      }
+    })
+    .done(function() {
+      console.log("Creando Objeto Pelicula");
+      crearPelicula();
+    })
+    .fail(function() {
+      console.log("Error, no se ha podido encontrar la pelicula: "+ imdbID);
+    });
+}
+
+function crearPelicula() {
+  temp = new Pelicula(jsonPelicula.Title,
+    jsonPelicula.Year,
+    jsonPelicula.imdbID,
+    jsonPelicula.Poster);
+  temp.extendido(jsonPelicula.Rated,
+    jsonPelicula.Runtime,
+    jsonPelicula.Genre,
+    jsonPelicula.Director,
+    jsonPelicula.Plot,
+    jsonPelicula.imdbRating)
+  // console.log(temp);
+  contenedorPeliculas.push(temp);
+}
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// + Toda la magia de jQuery esta aquí
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+$(document).ready(function() {
+  // console.log('Ahora carga');
+  $('.busqueda').on('click', '#buscar', function(event) {
+    event.preventDefault();
+    busqueda = $(this).closest('.busqueda').find('input').val();
+    if (!busqueda=='')
+    {
+      jsonBusqueda = [];
+      contadorPaginas = 1;
+      contenedorPeliculas = [];
+      ajaxBusqueda(contadorPaginas);
+      contadorPaginas++;
+    }
+  });
+  $('.busqueda').on('click', '#mas', function(event) {
+    event.preventDefault();
+    if (contadorPaginas <= maxPagina) {
+      ajaxBusqueda(contadorPaginas);
+      contadorPaginas++;
+    }
+
+  });
+});
