8b396608f6e0df1d1660c5b119b4a052594f5627
Manuel Muñoz <heidiaricel@hotmail.com>
2018-01-17 14:06:51 +0100

    My-app v0.0.3
    - Prueba de autenticación de la Api de las cervezas
    - Añadido la URL para poder obtener el JSON desde la web de antonio, MUCHAS GRACIAS POR TU AYUDA @ihton
    - Prueba para acceder a los parametros de una cerveza con sus valores

--- TEMA_8/Ejercicio_0_Test/my-app/src/app/prueba-servicio.service.ts
+++ TEMA_8/Ejercicio_0_Test/my-app/src/app/prueba-servicio.service.ts
@@ -3,26 +3,37 @@ import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
 
 @Injectable()
 export class PruebaServicioService {
+  apiAntonio: string;
+  apiCabecera: string;
+  apiKey: string;
+  apiFormato: string;
+  apiBusqueda: string;
+
   cars = ["bmw", "audi", "vw"];
-  datos: any;
+
+  datos: object;
+
   constructor(private contenedorAjax: HttpClient) {
     this.datos = []
+    this.apiAntonio = 'https://atalgaba.com/api.php?url=';
+    this.apiCabecera = 'https://api.brewerydb.com/v2/';
+    this.apiKey = '735852ebc132a2ac2b9c12c8a9ddd37d';
+    this.apiFormato = 'json';
+    this.apiBusqueda = 'beer/eGtqKZ';
   }
 
   getDatos() {
-    // this.contenedorAjax.get('https://api.brewerydb.com/v2/beer/d2l7om', {
-    // headers: new HttpHeaders().set('key', '735852ebc132a2ac2b9c12c8a9ddd37d'),
-    // params: new HttpParams().set('format','json'),
-    //   params: new HttpParams().set('key', '735852ebc132a2ac2b9c12c8a9ddd37d').set('format','json').set('callback','foo'),
-    // }).subscribe(data => {
+    this.contenedorAjax.get('' + this.apiAntonio + '' + this.apiCabecera + '' + this.apiBusqueda + '?key=' + this.apiKey + '&format=' + this.apiFormato + '').subscribe(data => {
+      console.log('Respuesta Get');
+      console.log(data);
+      this.datos = data;
+      console.log('Valor de la variable this.datos');
+      console.log(this.datos);
+    });
+    // this.contenedorAjax.get('http://www.omdbapi.com/?apikey=9d074a1f&s=batman&type=movie&page=1').subscribe(data => {
     //   console.log(data);
     //   this.datos = data;
-    //   console.log(data);
     // });
-
-    this.contenedorAjax.get('http://www.omdbapi.com/?apikey=9d074a1f&s=batman&type=movie&page=1').subscribe(data => {
-      console.log(data);
-    });
   }
 
 }
