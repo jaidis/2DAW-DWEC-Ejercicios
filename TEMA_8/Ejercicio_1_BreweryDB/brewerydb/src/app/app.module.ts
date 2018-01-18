4c5ab5ce6661ab8ab82b62f5febd3ac67d4af8ab
Manuel Muñoz <heidiaricel@hotmail.com>
2018-01-18 19:43:18 +0100

    Brewery DB Api v.0.0.3
    - Se ha activado el routeo de enlaces (ya se puede pasar entre componentes)
    - Se han creado los componentes cerveceria y cerveza (ambos componentes serán una plantilla en la que se cargará la información de una cerveceria o una cerveza ofreciendo mayor información)

--- TEMA_8/Ejercicio_1_BreweryDB/brewerydb/src/app/app.module.ts
+++ TEMA_8/Ejercicio_1_BreweryDB/brewerydb/src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { TemplateComponent } from './template/template.component';
+
+// importaciones realizadas
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { CerveceriaComponent } from './cerveceria/cerveceria.component';
+import { CervezaComponent } from './cerveza/cerveza.component';
+
+//routas del sitio web BreweryDB
+const routes: Routes = [
+  { path: '', component: BodyComponent },
+  { path: 'plantilla', component: TemplateComponent },
+  { path: 'cerveceria', component: CerveceriaComponent },
+  { path: 'cerveza', component: CervezaComponent },
+  { path: '**', component: BodyComponent }
+];
+export const routing = RouterModule.forRoot(routes);
+// fin declaración rutas sitio web BreweryDB
 
 @NgModule({
   declarations: [
@@ -15,11 +31,14 @@ import { HttpClientModule } from '@angular/common/http';
     HeaderComponent,
     BodyComponent,
     FooterComponent,
-    TemplateComponent
+    TemplateComponent,
+    CerveceriaComponent,
+    CervezaComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
