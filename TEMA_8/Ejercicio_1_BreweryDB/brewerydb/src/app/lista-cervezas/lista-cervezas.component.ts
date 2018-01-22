c5b63f11c54d5e0e595ceb9aed21a638bdc416c4
Manuel Muñoz <heidiaricel@hotmail.com>
2018-01-22 19:14:09 +0100

    BreweryDB Api v0.0.8.1
    - Añadidas mas cervezas a la lista de cervezas del mundo
    - Añadido al carrousel los controles para pasar de foto en el body

--- TEMA_8/Ejercicio_1_BreweryDB/brewerydb/src/app/lista-cervezas/lista-cervezas.component.ts
+++ TEMA_8/Ejercicio_1_BreweryDB/brewerydb/src/app/lista-cervezas/lista-cervezas.component.ts
@@ -10,18 +10,26 @@ export class ListaCervezasComponent implements OnInit {
   cervezasDelMundo: Array<Array<string>>;
   constructor(private servicio: ServicioApiService) {
     this.cervezasDelMundo = [
-      ['Heineken', 'eGtqKZ', 'https://s3.amazonaws.com/brewerydbapi/beer/eGtqKZ/upload_7HtBRP-large.png'],
-      ['Budweiser', '1P45iR', 'https://s3.amazonaws.com/brewerydbapi/beer/1P45iR/upload_Y13vwL-large.png'],
-      ['Miller Lite', 'KJIjyd', 'https://s3.amazonaws.com/brewerydbapi/beer/KJIjyd/upload_c3IJel-large.png'],
-      ['Corona Extra', 'ujPz4L', 'https://s3.amazonaws.com/brewerydbapi/beer/ujPz4L/upload_caVtLx-large.png'],
-      ['Paulaner', 'L50oJn', 'https://s3.amazonaws.com/brewerydbapi/beer/L50oJn/upload_LY8JoD-large.png'],
-      ['Warsteiner', 'Fcz43Y', 'https://s3.amazonaws.com/brewerydbapi/beer/Fcz43Y/upload_VKx4oD-large.png'],
-      ['Erdinger Weißbier', 'zVwJPQ', 'https://s3.amazonaws.com/brewerydbapi/beer/zVwJPQ/upload_neVGXs-large.png'],
-      ['Beck´s', 'IDlepc', 'https://s3.amazonaws.com/brewerydbapi/beer/IDlepc/upload_3zlBuG-large.png'],
-      ['Leffe Brune', 'AGgBSb', 'https://s3.amazonaws.com/brewerydbapi/beer/AGgBSb/upload_d2hAWz-large.png'],
-      ['Mahou', 'Omi2cO', 'https://s3.amazonaws.com/brewerydbapi/beer/Omi2cO/upload_FVqsYS-large.png'],
-      ['Amstel', 'CAkFaQ', 'https://s3.amazonaws.com/brewerydbapi/beer/CAkFaQ/upload_5uYBg6-large.png'],
-      ['Carlsberg', 'z7LxGT', 'https://s3.amazonaws.com/brewerydbapi/beer/z7LxGT/upload_YXlnlK-large.png']
+      ['Heineken', 'eGtqKZ', 'https://s3.amazonaws.com/brewerydbapi/beer/eGtqKZ/upload_7HtBRP-medium.png'],
+      ['Guinness Draught', 'StkEiv', 'https://s3.amazonaws.com/brewerydbapi/beer/StkEiv/upload_etArOb-medium.png'],
+      ['Amstel', 'CAkFaQ', 'https://s3.amazonaws.com/brewerydbapi/beer/CAkFaQ/upload_5uYBg6-medium.png'],
+      ['Carlsberg', 'z7LxGT', 'https://s3.amazonaws.com/brewerydbapi/beer/z7LxGT/upload_YXlnlK-medium.png'],
+      ['Paulaner', 'L50oJn', 'https://s3.amazonaws.com/brewerydbapi/beer/L50oJn/upload_LY8JoD-medium.png'],
+      ['Warsteiner', 'Fcz43Y', 'https://s3.amazonaws.com/brewerydbapi/beer/Fcz43Y/upload_VKx4oD-medium.png'],
+      ['Erdinger Weißbier', 'zVwJPQ', 'https://s3.amazonaws.com/brewerydbapi/beer/zVwJPQ/upload_neVGXs-medium.png'],
+      ['Leffe Brune', 'AGgBSb', 'https://s3.amazonaws.com/brewerydbapi/beer/AGgBSb/upload_d2hAWz-medium.png'],
+      ['Mahou', 'Omi2cO', 'https://s3.amazonaws.com/brewerydbapi/beer/Omi2cO/upload_FVqsYS-medium.png'],
+      ['Beck´s', 'IDlepc', 'https://s3.amazonaws.com/brewerydbapi/beer/IDlepc/upload_3zlBuG-medium.png'],
+      ['Budweiser', '1P45iR', 'https://s3.amazonaws.com/brewerydbapi/beer/1P45iR/upload_Y13vwL-medium.png'],
+      ['Miller Lite', 'KJIjyd', 'https://s3.amazonaws.com/brewerydbapi/beer/KJIjyd/upload_c3IJel-medium.png'],
+      ['Corona Extra', 'ujPz4L', 'https://s3.amazonaws.com/brewerydbapi/beer/ujPz4L/upload_caVtLx-medium.png'],
+      ['Pale Ale', 'cdkpyx', 'https://s3.amazonaws.com/brewerydbapi/beer/cdkpyx/upload_GUIYRA-medium.png'],
+      ['Yuengling Premium Beer', '2DdatF', 'https://s3.amazonaws.com/brewerydbapi/beer/2DdatF/upload_hzndMa-medium.png'],
+      ['Fat Tire', 'tuqTtX', 'https://s3.amazonaws.com/brewerydbapi/beer/tuqTtX/upload_L6CVSL-medium.png'],
+      ['Chimay Première', 'dKvrhI', 'https://s3.amazonaws.com/brewerydbapi/beer/dKvrhI/upload_qtCC8F-medium.png'],
+      ['Pabst Blue Ribbon', 'pDKyvz', 'https://s3.amazonaws.com/brewerydbapi/beer/pDKyvz/upload_UJqh68-medium.png'],
+      ['Coors Banquet', 'vJCXKD', 'https://s3.amazonaws.com/brewerydbapi/beer/vJCXKD/upload_E7cekQ-medium.png'],
+      ['Belgian White', 'dDXOEp', 'https://s3.amazonaws.com/brewerydbapi/beer/dDXOEp/upload_MOCbuS-medium.png']
     ];
   }
 
